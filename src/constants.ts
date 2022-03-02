6e12bc7d11ae8dd11b613c779309406fad4b7d42
Siris <vincentcmv@gmail.com>
2022-03-02 21:05:13 +0530

    refactor: djs

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,2 @@
 export const courses = ['4718', '4720', '4722', '4724', '4726', '4728', '4730', '4763'] as const;
-export const applicationID = '948216890428379217';
-export const publicKey = 'c07fbf6bd4465497a85727a2c6fcd358da00cda6f16cba816a03f4f2e492404b';
 export const mainServers = ['927078831381164033'];
