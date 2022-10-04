f9b4b40c92adbbedd616fab55a5d0dafa2c45847
Siris01 <40269790+Siris01@users.noreply.github.com>
2022-10-04 20:19:30 +0530

    fix: og tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,8 +10,8 @@ function MyApp({ Component, pageProps }: AppProps) {
 				<meta property='og:title' content='LMS' />
 				<meta property='og:description' content='View the upcoming assignments and get notified of future ones!' />
 				<meta property='og:author' content='Cyril' />
-				<meta property='theme-color' content='#https://coolors.co/5865f2' />
-				<meta name='theme-color' content='#https://coolors.co/5865f2' />
+				<meta property='theme-color' content='#5865f2' />
+				<meta name='theme-color' content='#5865f2' />
 				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
 				<meta name='description' content='View the upcoming assignments and get notified of future ones!' />
 			</Head>
