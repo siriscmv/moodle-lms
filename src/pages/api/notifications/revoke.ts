fe1d88cc7a63ad1cf121c10b242d2df8d17e6462
Siris01 <40269790+Siris01@users.noreply.github.com>
2022-10-05 16:52:34 +0530

    fix: dont create race conditions while revoking

--- src/pages/api/notifications/revoke.ts
+++ src/pages/api/notifications/revoke.ts
@@ -10,22 +10,10 @@ const handler: NextApiHandler = async (req, res) => {
 		return res.status(400).end();
 	}
 
-	db.notifications
-		.delete({
-			where: {
-				endpoint
-			}
-		})
-		.then(() => {
-			res.status(200).json({
-				success: true
-			});
-		})
-		.catch(() => {
-			res.status(200).json({
-				success: false
-			});
-		});
+	const deleted = await db.notifications.delete({ where: { endpoint } }).catch(() => null);
+
+	if (deleted) return res.status(200).json({ success: true });
+	return res.status(200).json({ success: false });
 };
 
 export default handler;
