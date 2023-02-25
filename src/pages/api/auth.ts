796ca77252f7bc6ec50a128c9afd329842cd5c9a
Siris01 <40269790+Siris01@users.noreply.github.com>
2023-02-25 17:53:27 +0530

    fix

--- src/pages/api/auth.ts
+++ src/pages/api/auth.ts
@@ -5,7 +5,10 @@ import passport from '@utils/passport';
 export default nextConnect().get(
 	passport.authenticate('google', { session: false }),
 	(req: NextApiRequest & { user: any }, res: NextApiResponse) => {
-		res.setHeader('Set-Cookie', `auth=${process.env.AUTH_COOKIE!}; Secure; HttpOnly; Max-Age=${7 * 24 * 60 * 60}`);
+		res.setHeader(
+			'Set-Cookie',
+			`auth=${process.env.AUTH_COOKIE!}; Path=/; Secure; HttpOnly; Max-Age=${7 * 24 * 60 * 60}`
+		);
 		res.redirect('/');
 	}
 );
