d7eeadfd464f7784cc80424f16651c6626c584d7
Siris01 <40269790+Siris01@users.noreply.github.com>
2023-03-24 18:41:43 +0530

    chore: change auth msg

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -5,7 +5,7 @@ const LoginPage = () => {
 		<div className='flex flex-col text-center my-auto mt-12 text-white'>
 			<h1 className='text-3xl font-bold text-primary'>Authentication</h1>
 			<span className='font-semibold text-lg p-2 m-2'>
-				You must authenticate yourself with the institution email account to access certain content
+				You must authenticate yourself with the institution email account to access certain resources
 			</span>
 			<Link href='/api/login' passHref>
 				<button className='bg-primaryBg border-2 p-4 m-4 rounded-md text-primary text-xl font-bold border-slate transition-all ease-in-out duration-150 hover:border-primary'>
