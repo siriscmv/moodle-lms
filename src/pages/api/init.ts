f6db8428c013f67427eb61e261ee15b883b2b362
Siris01 <40269790+Siris01@users.noreply.github.com>
2023-04-01 03:35:05 +0530

    feat: force option

--- src/pages/api/init.ts
+++ src/pages/api/init.ts
@@ -1,11 +1,20 @@
+import prisma from '@utils/db';
 import kv from '@utils/kv';
 import assignments from '@utils/syncAssignments';
 import files from '@utils/syncFiles';
 import type { NextApiHandler } from 'next';
+import { execSync } from 'node:child_process';
 
 const handler: NextApiHandler = async (req, res) => {
 	if (kv.get('init')) return res.status(404);
 	if (req.method !== 'GET') return res.status(405).end();
+
+	const force = req.query.force as string;
+
+	if (force) {
+		await prisma.files.deleteMany();
+		execSync('rm -rf ./files/* && mkdir ./files');
+	}
 
 	kv.set('init', true);
 	await assignments();
